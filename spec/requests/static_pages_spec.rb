e5812573fcfdf11a7111ec4f060c2585e053c470
Peter Harris <pharris@servicesource.com>
2012-10-17 16:18:40 -0700

    Adding default title to application pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,7 +10,8 @@ describe "StaticPages" do
 
     it "should have the title 'Home'" do
     	visit '/static_pages/home'
-    	page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Home")
+    	page.should have_selector('title', 
+        :text => "Ruby on Rails Tutorial Sample App | Home")
     end
   end
 
@@ -22,7 +23,8 @@ describe "StaticPages" do
 
   	it "should have the title 'Help'" do
     	visit '/static_pages/help'
-    	page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Help")
+    	page.should have_selector('title', 
+        :text => "Ruby on Rails Tutorial Sample App | Help")
     end
   end
 
@@ -34,7 +36,8 @@ describe "StaticPages" do
   	
   	it "should have the title 'About Us'" do
     	visit '/static_pages/about'
-    	page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | About Us")
+    	page.should have_selector('title', 
+        :text => "Ruby on Rails Tutorial Sample App | About Us")
     end
   end
 end
